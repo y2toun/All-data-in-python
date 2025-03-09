e282ecf62d5e95c7072ee7a87c17926da9f99f9b
Ahmed Yakout <ayakout228@gmail.com>
2025-03-09 23:37:13 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+#-------------------
+# type()
+# all data in python
+#-------------------
+
+print(type(10)) #int => integer 
+print(type(100)) #int => integer 
+print(type(-5)) #int => integer
+
+print(type(29.5)) #floet => floeting point num
+print(type(29.567546)) #floet => floeting point num
+print(type(-29.567546)) #floet => floeting point num
+
+print(type('gold')) #str => string
+
+print(type( [3, 4, 5, 6,] )) #list => list
+
+print(type((3, 3, 3,))) #tuple => tuple
+
+print(type({'one' : 1, 'two' : 2,})) #dict => dictionary
+
+print(type (2 == 2)) #bool => boolean
